ac2867ff877a0026c2ff125765a4c993ded5b095
wang.xin <wang.xin@uneed.com>
2021-11-10 15:52:42 +0800

    提交

--- main.go
+++ main.go
@@ -2,17 +2,14 @@ package main
 
 import (
 	"container/list"
-	"fmt"
 )
 
 func main() {
 	l := list.New()
 	first := l.PushFront("first")
 	last := l.PushBack("last")
-	l.InsertBefore("mid",last) //在 last前边插入元素
-	l.InsertAfter("second",first) // 在first后边插入一个元素
+	l.InsertBefore("mid", last)    //在 last前边插入元素
+	l.InsertAfter("second", first) // 在first后边插入一个元素
 	//遍历一下所有的元素
-	for i := l.Front();i != nil ;i = i.Next() {
-		fmt.Println(i.Value)
-	}
+
 }
