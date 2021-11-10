4b64b03a7022b771a7945f976a74fed36b6b21a5
wang.xin <wang.xin@uneed.com>
2021-11-10 15:54:06 +0800

    提交

--- main.go
+++ main.go
@@ -4,11 +4,14 @@ import (
 	"container/list"
 )
 
+//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
+
 func main() {
 	l := list.New()
 	first := l.PushFront("first")
 	last := l.PushBack("last")
 	l.InsertBefore("mid", last)    //在 last前边插入元素
 	l.InsertAfter("second", first) // 在first后边插入一个元素
+	//为啥每次都要username和password呢
 
 }
