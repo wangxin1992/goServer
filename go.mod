e998419350bc8958bdb60c596b68b7ac834cb2fc
wang.xin <wang.xin@uneed.com>
2021-11-18 19:04:43 +0800

    123

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module goTest
+module goServer
 
-go 1.16
+go 1.17
--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
+import (
+
+)
+
 func main() {
 	urls := fmt.Sprintf("https://restapi.amap.com/v5/ip?key=e93f35e859dfc4efc1f2bae4ac222cf2&type=4&ip=%s", "39.144.104.249")
 	response, _ := http.Get(urls)
 	body, _ := ioutil.ReadAll(response.Body)
 	defer response.Body.Close()
 	fmt.Println(string(body))
+	hello()
 }
 
-//
