bd19592378e9c3cba8ecbe01f2a5e2bf9cb914aa
wang.xin <wang.xin@uneed.com>
2021-11-18 18:10:47 +0800

    随便更新一下

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
+import "net/http"
+import "io/ioutil"
+
 
 
 func main() {
-	urls := fmt.Sprintf("https://restapi.amap.com/v5/ip?key=e93f35e859dfc4efc1f2bae4ac222cf2&type=4&ip=%s", "223.104.212.165")
+	urls := fmt.Sprintf("https://restapi.amap.com/v5/ip?key=e93f35e859dfc4efc1f2bae4ac222cf2&type=4&ip=%s", "39.144.104.249")
 	response, _ := http.Get(urls)
 	body, _ := ioutil.ReadAll(response.Body)
 	defer response.Body.Close()
